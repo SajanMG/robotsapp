47061b81cda6492e4eac4fc7177f9760cb715484
SajanG <sajantamu04@gmail.com>
2023-06-14 15:29:13 +1000

    adding and maitaining

---
+++ src/Containers/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import CardList from "../Components/CardList";
+import SearchBox from "../Components/SearchBox";
+import "./App.css";
+import Scroll from "../Components/Scroll";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    //using ternary operation her instead of normal
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1"> RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />;
+        </Scroll>
+      </div>
+    );
+
+    // if (!robots.length) {
+    //   return <h1>Loading</h1>;
+    // } else {
+    //   return (
+    //     <div className="tc">
+    //       <h1 className="f1"> RoboFriends</h1>
+    //       <SearchBox searchChange={this.onSearchChange} />
+    //       <Scroll>
+    //         <CardList robots={filteredRobots} />;
+    //       </Scroll>
+    //     </div>
+    //   );
+    // }
+  }
+}
+
+export default App;
